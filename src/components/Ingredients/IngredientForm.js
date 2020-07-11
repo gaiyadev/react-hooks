87970f7d823db78bc1754e84d6bedca8e0f31f82
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-07-12 00:18:37 +0100

    api call, adding n deleting items

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
+
+
 const IngredientForm = React.memo(props => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
+    props.onAddIngredient({ title: title, amount: amount })
   };
 
   return (
@@ -15,11 +21,22 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title"
+              onChange={event => {
+                const newTitle = event.target.value;
+                setTitle(newTitle);
+              }
+              }
+              value={title} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount"
+              onChange={event => {
+                const newAmount = event.target.value;
+                setAmount(newAmount)
+              }
+              } value={amount} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientsList from './IngredientList';
 
-function Ingredients() {
+const url = 'your firebase url';
+
+const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch(url + '/ingredients.json').then(response => {
+      return response.json();
+    }).then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setIngredients(loadedIngredients);
+    }).catch(err => console.log(err));
+  }, [])
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(url + '/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      return response.json();
+
+    }).then(responseData => {
+      setIngredients(prevIngredient => [...prevIngredient,
+      {
+        id: responseData.name,
+        ...ingredient
+      }
+      ]);
+    }).catch(err => console.log(err));
+
+    setIngredients(prevIngredient => [...prevIngredient,
+    {
+      id: Math.random().toString(),
+      ...ingredient
+    }
+    ]);
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setIngredients(prevIngredient => prevIngredient.filter(ingredient => ingredient.id
+      !== ingredientId))
+  };
+
+
   return (
-    <div className="App">
-      <IngredientForm />
+    <div className="App" >
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+      />
 
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientsList ingredients={ingredients}
+          onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
